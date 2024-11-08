f65c986c13da35926121c1b39fa310cb682d8ca7
Vianney Carbel-Zekpa <hyperiya.vcz@outlook.com>
2024-11-08 12:53:03 -0500

    FRONT-BACKEND INTEGRATION MIGHT WORK OMGOMGOMG LETS FUCKING GOOOOOOOOOO

--- ZZZApp/Python/main.py
+++ ZZZApp/Python/main.py
@@ -5,12 +5,33 @@ from pathlib import Path
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
-
+from PySide6.QtCore import QObject, Signal, Slot, Property
 from autogen.settings import url, import_paths
+
+class Backend(QObject):
+    dataChanged = Signal()
+
+    def __init__(self):
+        super().__init__()
+        self._data = "Initial value"
+
+    @Property(str, notify=dataChanged)
+    def data(self):
+        return self._data
+
+    @Slot()
+    def updateData(self):
+        self._data = "Updated value"
+        self.dataChanged.emit()
+
+
 
 if __name__ == '__main__':
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
+    
+    backend = Backend()
+    engine.rootContext().setContextProperty("backend", backend)
 
     app_dir = Path(__file__).parent.parent
 
